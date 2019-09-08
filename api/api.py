0e10a4f426f23baf3cea0fb59bc2e2657d7f2c50
JKSenthil <senthil.jason@gmail.com>
2019-09-07 23:22:26 -0500

    api code pt1

--- api/api.py
+++ api/api.py
@@ -1,1 +1,22 @@
+import json
+import requests
 import numpy as np
+import pandas as pd
+
+#lat = "26.8467"
+#long = "80.9462"
+lat = "28.3670"
+long = "79.4304"
+time = "1391230800"
+#time = 1389939200
+#?exclude=hourly,daily
+
+key = "c9691deebbbe8f7538f597fb79afc319"
+url = "https://api.darksky.net/forecast/{}/{},{},{}".format(key, lat, long, time)
+
+data = requests.get(url)
+tru = json.loads(data.text)
+print(tru.keys())
+print(tru['currently'])
+
+#print(data)
