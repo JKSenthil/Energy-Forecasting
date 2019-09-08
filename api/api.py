979d42079dcc4ee043516731d22fa788a77260b9
Nimish Garg <nimish_garg@hotmail.com>
2019-09-08 16:58:14 -0400

    first git commit to project

--- api/api.py
+++ api/api.py
@@ -3,20 +3,21 @@ import requests
 import numpy as np
 import pandas as pd
 
-#lat = "26.8467"
-#long = "80.9462"
-lat = "28.5355"
-long = "77.3910"
-time = "1391565600"
-#time = 1389939200
-#?exclude=hourly,daily
+KEY = "c9691deebbbe8f7538f597fb79afc319"
 
-key = "c9691deebbbe8f7538f597fb79afc319"
-url = "https://api.darksky.net/forecast/{}/{},{},{}".format(key, lat, long, time)
+cities = {}
+cities["Lucknow"] = {"lat": 26.7617, "long": 80.8857}
+cities["Unnao"] = {"lat": 26.5393, "long": 80.4878}
+cities["Noida"] = {"lat": 28.3590, "long": 77.5508}
+
+data = {}
+
+for city in cities:
+    for time in range(1388514600, 1567708200 + 86400, 86400):
+        url = "https://api.darksky.net/forecast/{}/{},{},{}".format(KEY, cities[city]["lat"], cities[city]["long"], time)
+
+
+url = "https://api.darksky.net/forecast/{}/{},{},{}".format(KEY, lat, long, time)
 
 data = requests.get(url)
 tru = json.loads(data.text)
-print(tru.keys())
-print(tru['currently'])
-
-#print(data)
