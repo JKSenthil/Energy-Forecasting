d49db8685fc77ca54b7cf38c6b3635d0ea79d001
Neev Parikh <neev_parikh@brown.edu>
2019-09-22 23:30:52 -0400

    Added some points

--- src/train.py
+++ src/train.py
@@ -1,1 +1,16 @@
-import numpy as np
+import pandas as pd
+import xgboost as xgb
+
+# read in data
+dtrain = xgb.DMatrix('../data/Lucknow_Weather.csv')
+dtest = xgb.DMatrix('../data/Lucknow_Weather.csv')
+
+# specify parameters via map
+param = {'max_depth':2, 'eta':1, 'objective':'binary:logistic' }
+num_round = 2
+bst = xgb.train(param, dtrain, num_round)
+
+# make prediction
+preds = bst.predict(dtest)
+
+
