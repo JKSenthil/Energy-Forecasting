0b2362112ac3af22533f3957978950f04a99cd0f
Nimish Garg <nimish_garg@hotmail.com>
2020-03-26 12:05:04 -0400

    pls work for god's sake

--- src/train.py
+++ src/train.py
@@ -18,15 +18,22 @@ from pandas import concat
 from pandas import DataFrame
 
 
-data = pickle.load(open("../data/data.p", "rb"))
+everything = pickle.load(open("../data/data.p", "rb"))
 
-data = data[0]
+
+data = everything[0]
 data = data[['Unix', 'Demand','CloudCoveragePercent', 'SurfaceTemperatureCelsius', 'SurfaceDewpointTemperatureCelsius',
          'RelativeHumidityPercent', 'SurfaceAirPressureKilopascals', 'ApparentTemperatureCelsius',
          'WindChillTemperatureCelsius', 'WindSpeedKph', 'WindDirectionDegrees']]
 
 data['Unix'] = pd.to_datetime(data['Unix'],unit='s')
-data.to_csv('raw.csv', index=False)
+
+x = pd.concat([data, everything[1].drop(columns = ['Unix', 'Demand']), everything[2].drop(columns = ['Unix', 'Demand']), 
+               everything[3].drop(columns = ['Unix', 'Demand']), 
+               everything[4].drop(columns = ['Unix', 'Demand']), everything[5].drop(columns = ['Unix', 'Demand']), 
+               everything[6].drop(columns = ['Unix', 'Demand']), everything[7].drop(columns = ['Unix', 'Demand'])], axis = 1)
+print(x.shape)
+x.to_csv('raw.csv', index=False)
 dataset = read_csv('raw.csv', index_col=0)
 dataset.index.name = 'Unix'
 print(dataset.head(5))
@@ -67,87 +74,37 @@ def series_to_supervised(data, n_in=1, n_out=1, dropnan=True):
 	return agg
 
 dataset = read_csv('pollution.csv', header=0, index_col=0)
+print(dataset.shape)
 values = dataset.values
 values = values.astype('float32')
-# scaler = MinMaxScaler(feature_range=(0, 1))
-# scaled = scaler.fit_transform(values)
-scaled = values
 n_hours = 360
-n_features = 10
-reframed = series_to_supervised(scaled, n_hours, 1)
-reframed.drop(reframed.columns[[-9, -8, -7, -6, -5, -4, -3, -2, -1]], axis=1, inplace=True)
-print(reframed.head())
+n_features = 72
+look_ahead = 154
+reframed = series_to_supervised(values, n_hours, look_ahead)
 
 #Using Multilayer Perceptron
 #10 * 360 * data points
 arr = reframed.to_numpy()
-Y = arr[:, 3600]
-arr = arr[:, :3600]
-X = np.reshape(arr, (-1, n_hours, n_features))
+print(arr.shape)
+y_value = []
+for x in range(1, 155):
+  y_value.append(-x * 10 + 5140)
+n_input = 4987
 
-n_input = X.shape[1] * X.shape[2]
-X = X.reshape((X.shape[0], n_input))
-# define model
+X = np.delete(arr, y_value, axis = 1)
+Y = arr[:, y_value]
+
 model = Sequential()
-model.add(Dense(100, activation='relu', input_dim=n_input))
-model.add(Dense(1))
+model.add(Dense(100, activation='relu', input_dim = n_input - 1))
+model.add(Dense(154))
 model.compile(optimizer='adam', loss='mse')
 # fit model
 model.fit(X, Y, epochs=5, verbose=0)
 
-
-# # print(data[0]["Unix"])
-# # a = 0
-
-# # index2city = {
-# #     'Bhagalpur': 0,
-# #     'Bhojpur' : 1,
-# #     'East Champaran' : 2,
-# #     'Kishanganj' : 3,
-# #     'Munger' : 4,
-# #     'Muzzafarpur' : 5,
-# #     'Nalanda' : 6,
-# #     'Patna' : 7,
-# #     'Rohtas' : 8,
-# #     'Vaishali' : 9
-# # }
-
-# # # data[0] -> dataframe, data[0]["Unix"]
-# # data[0]["Unix"] = data[0]["Unix"] % 1440
-# # columns = ['CloudCoveragePercent', 'SurfaceTemperatureCelsius', 'SurfaceDewpointTemperatureCelsius',
-# #          'RelativeHumidityPercent', 'SurfaceAirPressureKilopascals', 'ApparentTemperatureCelsius',
-# #          'WindChillTemperatureCelsius', 'WindSpeedKph', 'WindDirectionDegrees', 'Demand']
-
-# # # Extract Patna data for now
-
-
-# # processed_data = data[index2city['Patna']].to_numpy()
-# # X = processed_data[:,1:-1]
-# # y = processed_data[:,-1]
-
-# # length = len(X)
-# # x_groups = []
-# # y_groups = []
-# # for i in range(10, length):
-# #     x_data = [X[(i-10):(i)]]
-# #     # y_data = [y[i:(i+11)]]
-# #     x_groups.append(x_data)
-# #     # y_groups.append(y_data)
-# # y = y[10:]
-
-# # print(len(x_groups))
-# # print(len(y))
-
-# # import tensorflow
-# # from tensorflow.keras.models import Sequential
-# # from tensorflow.keras.layers import LSTM, Dense
-
-# # model = Sequential()
-# # model.add(LSTM(50))
-# # model.add(Dense(1))
-# # model.compile(loss='mae', optimizer='adam')
-
-# # model.fit(x_groups, y, epochs=50, batch_size=100, shuffle=False)
-
-# # #Input arrays should have the same number of samples as target arrays. Found 1 input samples and 132714 target samples.
- 
+n = 9000
+test = X[n, :]
+test = test.reshape(1, n_input - 1)
+actual_y = Y[n]
+print(actual_y)
+yhat = model.predict(test, verbose=0)
+print(yhat)
