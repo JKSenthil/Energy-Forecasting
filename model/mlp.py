bed1442bb09c5378e963699543360f97e10fb22a
JKSenthil <senthil.jason@gmail.com>
2020-04-25 21:20:35 -0500

    L

--- model/mlp.py
+++ model/mlp.py
@@ -6,7 +6,7 @@ class BasicMLP(nn.Module):
     Neural Network which only uses current weather to
     make immediate energy demand prediction, 1 time step
     """
-    def __init__(self, prev_len=10*3*96, curr_len=10*96, n_out=96):
+    def __init__(self, prev_len, curr_len, n_out):
         super(BasicMLP, self).__init__()
 
         self.model = nn.Sequential(
--- train.py
+++ train.py
@@ -20,7 +20,7 @@ def train_test_split(data, percentage=0.8):
     """
     n = len(data)
     train_size = int(n * percentage)
-    return data[:train_size], data[train_size:]
+    return data[:train_size,:], data[train_size:,:]
 
 def random_train_test_split(data, percentage=0.8):
     """
@@ -40,18 +40,10 @@ def train_basicMLP(model, optimizer, loss_function, data, num_epochs, batch_size
     batch_size: simultanous data model trains on (right value can speed up model convergence)
     n_out: number of timesteps to predict demand for
     """
-    # load autoencoder models
-    autoencoder1 = AutoEncoder(len(data[0]), 10)
-    autoencoder1.load_state_dict(torch.load(EXPERIMENTS_DIR + "/ac.pth"))
-    autoencoder2 = AutoEncoder(len(data[0])-1, 10)
-    autoencoder2.load_state_dict(torch.load(EXPERIMENTS_DIR + "/ac_drop_demand.pth"))
-    autoencoder1.to(device)
-    autoencoder2.to(device)
-
     # initialize training infrastructure vars
     indicies = np.arange(n_prev, len(data)-n_out)
     num_batches = (len(data) // batch_size) - batch_size
-    input_size = 10*3*96
+    prev_size = len(data)*7*96
     for i in range(num_epochs):
         print("Starting epoch {}".format(i+1))
         
@@ -60,15 +52,15 @@ def train_basicMLP(model, optimizer, loss_function, data, num_epochs, batch_size
 
         for j in range(num_batches):
             # initialize batch data array
-            batch_X = torch.zeros((batch_size, input_size)).to(device)
-            batch_curr_weather = torch.zeros((batch_size, n_out*10)).to(device)
-            batch_Y = np.zeros((batch_size, 96))
+            batch_X = torch.zeros((batch_size, prev_size)).to(device)
+            batch_curr_weather = torch.zeros((batch_size, n_out*(len(data) - 1))).to(device)
+            batch_Y = np.zeros((batch_size, n_out))
             for k in range(j*batch_size,(j+1)*batch_size):
                 l = indicies[k]
                 _prev_data = torch.from_numpy(data[l-n_prev:l,:]).float().to(device)
-                batch_X[k % batch_size, :] = torch.flatten(autoencoder1.enc(_prev_data))
+                batch_X[k % batch_size, :] = _prev_data
                 _curr_data = torch.from_numpy(data[l:l+n_out, :-1]).float().to(device)
-                batch_curr_weather[k % batch_size, :] = torch.flatten(autoencoder2.enc(_curr_data))
+                batch_curr_weather[k % batch_size, :] = _curr_data
 
                 batch_Y[k % batch_size, :] = data[l:l+n_out, -1]
 
@@ -77,7 +69,7 @@ def train_basicMLP(model, optimizer, loss_function, data, num_epochs, batch_size
             # compute backprop for model
             optimizer.zero_grad()
             y_pred = model(batch_X, batch_curr_weather)
-            loss = loss_function(batch_Y, y_pred)
+            loss = loss_function((batch_Y * _max) + _min, (y_pred * _max) + _min)
             loss.backward()
             optimizer.step()
             
@@ -216,14 +208,14 @@ def train_crnn(model, optimizer, loss_function, data, num_epochs, batch_size, n_
             
             print("Batch {} of {} done, loss={}".format(j+1, num_batches, loss))
 
-def train_crnnV2(model, ae_model, optimizer, loss_func, data, num_epochs, batch_size, n_prev, n_out):
+def train_crnnV2(model, ae_model, optimizer, loss_func, data, test_data, num_epochs, batch_size, n_prev, n_out):
     indicies = np.arange(n_prev, len(data)-n_out)
     num_batches = (len(data) // batch_size) - batch_size
     lag_size = 1
     curr_size = len(data[0]) - 1
 
     for i in range(num_epochs):
-        print("Starting epoch {}".format(i))
+        print("Starting epoch {}".format(i+1))
 
         # shuffles indicies to shuffle training data order
         np.random.shuffle(indicies)
@@ -245,18 +237,44 @@ def train_crnnV2(model, ae_model, optimizer, loss_func, data, num_epochs, batch_
             # convert inputs to torch tensors
             batch_lag = torch.from_numpy(batch_lag).float().to(device)
             batch_curr = torch.from_numpy(batch_curr).float().to(device)
-            enc_batch_curr = ae_model.enc(batch_curr)
+            # enc_batch_curr = ae_model.enc(batch_curr)
             batch_Y = torch.from_numpy(batch_Y).float().to(device)
 
             # compute backprop for model
             optimizer.zero_grad()
-            y_pred = model.forward(batch_lag, enc_batch_curr)
-            loss = loss_function((batch_Y * _max) + _min, (y_pred * _max) + _min)
+            y_pred = model.forward(batch_lag, batch_curr)
+            loss = loss_func((batch_Y * _max) + _min, (y_pred * _max) + _min)
             loss.backward()
             optimizer.step()
             
             print("Batch {} of {} done, loss={}".format(j+1, num_batches, loss))
 
+        # computing testing loss
+        test_indicies = np.arange(n_prev, len(test_data)-n_out)
+        test_num_batches = (len(test_data) // 50) - 50
+        total_loss = 0
+        for j in range(test_num_batches):
+            test_batch_lag = np.zeros((50, n_prev, lag_size))
+            test_batch_curr = np.zeros((50, n_out, curr_size))
+            test_batch_Y = np.zeros((50, n_out))
+            for k in range(j * 50, (j+1)*50):
+                l = test_indicies[k]
+                test_batch_lag[k % 50, :, :] = np.expand_dims(test_data[l-n_prev:l, -1], 1)
+                test_batch_curr[k % 50, :, :] = test_data[l:l+n_out, :-1]
+                test_batch_Y[k % 50, :] = test_data[l:l+n_out, -1]
+            test_batch_lag = np.swapaxes(test_batch_lag, 0, 1)
+            test_batch_curr = np.swapaxes(test_batch_curr, 0, 1)
+
+            test_batch_lag = torch.from_numpy(test_batch_lag).float().to(device)
+            test_batch_curr = torch.from_numpy(test_batch_curr).float().to(device)
+            # enc_test_batch_curr = ae_model.enc(test_batch_curr)
+            test_batch_Y = torch.from_numpy(test_batch_Y).float().to(device)
+        
+            # compute testing loss
+            test_y_pred = model.forward(test_batch_lag, test_batch_curr)
+            test_loss = loss_func((test_batch_Y * _max) + _min, (test_y_pred * _max) + _min).item()
+            total_loss += test_loss
+        print("Epoch {} done, loss={}".format(i+1, total_loss / test_num_batches))
 
 # ================ TRAIN AUTOENCODER ======================== #
 # data = load_formatted_datav3()
@@ -279,23 +297,27 @@ def train_crnnV2(model, ae_model, optimizer, loss_func, data, num_epochs, batch_
 # =================================================== #
 
 # ================ TRAIN crnnV2 ======================== #
-enc_weather_size = 10
-data, _max, _min = load_formatted_datav3()
-weather_ae = AutoEncoder(len(data[0]) - 1, enc_weather_size)
-weather_ae.load_state_dict(torch.load(EXPERIMENTS_DIR + "/weather_ae.pth"))
-weather_ae.to(device)
-model = cRNNv2(1, enc_weather_size, 96, 128)
-model.to(device)
-optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
-loss_function = nn.MSELoss().to(device)
-train_crnnV2(model, weather_ae, optimizer, loss_function, data, 100, 128, 96 * 5, 96)
-# =================================================== #
+# num_days = 10/96
 
-# ================ TRAIN MLP ======================== #
-# data = load_formatted_datav3()
-# model = BasicMLP()
+# data, _max, _min = load_formatted_datav2()
+# data = data[:,[-5,-1]]
+# enc_weather_size = len(data[0]) - 1
+# train_data, test_data = train_test_split(data, percentage=0.90)
+# weather_ae = None #AutoEncoder(len(data[0]) - 1, 10)
+# #weather_ae.load_state_dict(torch.load(EXPERIMENTS_DIR + "/weather_ae.pth"))
+# #weather_ae.to(device)
+# model = cRNNv2(1, enc_weather_size, 64, 32)
 # model.to(device)
 # optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
 # loss_function = nn.MSELoss().to(device)
-# train_basicMLP(model, optimizer, loss_function, data, 10, 64)
+# train_crnnV2(model, weather_ae, optimizer, loss_function, train_data, test_data, 100, 128, 96 * 5, 96)
+# =================================================== #
+
+# ================ TRAIN MLP ======================== #
+data, _max, _min = load_formatted_datav3()
+model = BasicMLP(len(data)*96, 96 * (len(data) - 1), 96 * (len(data) - 1))
+model.to(device)
+optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
+loss_function = nn.MSELoss().to(device)
+train_basicMLP(model, optimizer, loss_function, data, 10, 64)
 # =================================================== #
